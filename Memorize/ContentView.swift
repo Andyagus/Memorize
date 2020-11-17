5caa7f8c503940b4c5ed53b72a280e1370b6409d
Andy Agus <Redagus@gmail.com>
2020-11-17 15:25:44 -0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Andy Agus on 11/17/20.
+//  Copyright © 2020 andyagus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
