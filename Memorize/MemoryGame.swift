f65dc3a2ee9fe8d39944279cc722029f8be0a8f1
Andy Agus <Redagus@gmail.com>
2020-11-24 16:08:09 -0500

    recoding - lecture

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -2,23 +2,8 @@
 //  MemoryGame.swift
 //  Memorize
 //
-//  Created by Andy Agus on 11/18/20.
+//  Created by Andy Agus on 11/24/20.
 //  Copyright © 2020 andyagus. All rights reserved.
 //
 
 import Foundation
-
-struct MemoryGame<CardContent>{
-    var cards: Array<Card>
-    
-    func choose(card: Card) {
-        print("card chosen: \(card)")
-    }
-    
-    struct Card{
-        var isFaceUp: Bool
-        var isMatched: Bool
-        var content: CardContent
-    }
-}
-
